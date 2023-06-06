0b33a34d1dd7a66bfad77fcb3631b5b69d7fb7b3
Mohemed ansad mk <92428126+ansadmk@users.noreply.github.com>
2023-06-06 11:09:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/themeChangerUsingReact-wk_01_Day_03-"
+  base:"/themeChangerUsingReact-wk_01_Day_03-/"
 })
